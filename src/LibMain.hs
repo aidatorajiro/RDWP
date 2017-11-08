8eda294a9c04eea6484357c36d37877497202cec
aidatorajiro <kawarusosu@zoho.com>
2017-11-08 16:04:55 +0900

    Routing fix

--- src/LibMain.hs
+++ src/LibMain.hs
@@ -7,12 +7,13 @@ import Location
 
 import qualified Index
 import qualified FakeIndex
+import qualified Mensae
 import qualified Error404
 
 router :: String -> IO ()
 router "index" = Index.page
 router "" = FakeIndex.page
-router "mensae" = Index.page
+router "mensae" = Mensae.page
 router _ = Error404.page
 
 startApp :: IO ()
