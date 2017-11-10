f0449175203f909299a7affe2633c9bcf556d9f1
aidatorajiro <kawarusosu@zoho.com>
2017-11-10 18:19:06 +0900

    Revert "Add comments.". Silence is golden.
    This reverts commit 5f4dfc32dd24630eff6499f3119d141a4b3ceff9.

--- src/Index.hs
+++ src/Index.hs
@@ -14,22 +14,12 @@ import Reflex.Dom
 import Util ( randomRDyn, getTickCount )
 import Location ( move )
 
--- This page consists of two elements, "Hello, World!" text and progress bar.
--- The style of "Hello, World!" text changes at random.
--- The progress bar increases constantly, and when it complete, users jumped to "mensae".
-
 page :: IO ()
 page = mainWidget $ do
-  -- get the current tick count
   tickcnt <- getTickCount
 
-  -- get the initial random generator
   initGen <- liftIO getStdGen
-
-  -- get the random generator which will be updated at each tick
   (rnd, gen) <- randomRDyn (1 :: Int, 800) initGen (updated tickcnt)
-
-  -- define the style of "Hello, World!" text
   style <- foldDynMaybe (\r _ -> case r of
     400 -> Just "text-align: center;"
     255 -> Just "color: #FFFFFF;"
@@ -40,14 +30,10 @@ page = mainWidget $ do
     111 -> Just ""
     _   -> Nothing) "" (updated rnd)
 
-  let -- calculate the percentage of the length of the progress bar.
-      percentage = min 100 . ( * 0.01 ) <$> tickcnt
-      -- calculate the attribute for the "Hello, world!" text.
+  let percentage = min 100 . ( * 0.01 ) <$> tickcnt
       attr_h1 = (\s -> M.fromList [("style", s)]) <$> style
-      -- calculate the attribute for the progress bar.
       attr_div = (\p -> M.fromList [("style", T.pack $ "height: 1em; width: " ++ show p ++ "%; background: black;")]) <$> percentage
 
-  -- When the progress bar complete, users will jump to "mensae".
   dyn =<< foldDynMaybe
     (\t _ ->
       if t == 10000 then
@@ -56,6 +42,5 @@ page = mainWidget $ do
     (return ())
     (updated tickcnt)
 
-  -- elements
   elDynAttr "h1" attr_h1 $ text "Hello, world!"
   elDynAttr "div" attr_div blank
