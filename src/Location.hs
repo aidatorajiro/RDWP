d1abeb632d4b658c6821d95a84a78a503ea4c7bf
aidatorajiro <kawarusosu@zoho.com>
2017-11-11 15:05:59 +0900

    add asset

--- src/Location.hs
+++ src/Location.hs
@@ -1,12 +1,18 @@
-module Location ( location, move ) where
+module Location ( location, move, path ) where
 
 import Data.JSString ( JSString, unpack, pack )
 
 foreign import javascript safe "var m = location.pathname.match('^/([^/]+)$'); if (m === null) { $r = '' } else { $r = m[1] };" locationJSString :: IO JSString
 foreign import javascript safe "location.href = '/' + $1" moveJSString :: JSString -> IO ()
 
+-- jump to specific location
 move :: String -> IO ()
 move = moveJSString . pack
 
+-- get current location
 location :: IO String
 location = unpack <$> locationJSString
+
+-- get asset path
+asset :: String -> String
+asset = ("/assets/" ++)
