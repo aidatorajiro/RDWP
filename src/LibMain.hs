b585731acca1dfeb7f05fc4b1d03dbcacaa20024
aidatorajiro <kawarusosu@zoho.com>
2018-01-09 20:38:11 +0900

    use parsec

--- src/LibMain.hs
+++ src/LibMain.hs
@@ -1,20 +1,27 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, FlexibleContexts #-}
 
 module LibMain ( startApp ) where
 
 import Reflex.Dom
 import Location
+import Text.Parsec
 
 import qualified Index
 import qualified FakeIndex
 import qualified Mensae
 import qualified Error404
 
+url :: Stream s m Char => String -> IO () -> ParsecT s u m (IO ())
+url x m = string x >> eof >> return m
+
+pathParser :: Stream s m Char => ParsecT s u m (IO ())
+pathParser   =   (url "index" Index.page)
+            <|>  (url "" FakeIndex.page)
+            <|>  (url "mensae" Mensae.page)
+            <|>  (return Error404.page)
+
 router :: String -> IO ()
-router "index" = Index.page
-router "" = FakeIndex.page
-router "mensae" = Mensae.page
-router _ = Error404.page
+router s = either (fail "url parse error") id (parse pathParser "" s)
 
 startApp :: IO ()
 startApp = router =<< location
---
+++ src/Source.hs
@@ -0,0 +1,1 @@
+module Source where
