316d46c17db3232956a34985c88904bc9b1615b2
aidatorajiro <kawarusosu@zoho.com>
2018-09-10 20:12:06 +0900

    change Mensae.hs

--- frontend/src/Mensae.hs
+++ frontend/src/Mensae.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecursiveDo #-}
 
 module Mensae ( page ) where
 
@@ -6,24 +7,31 @@ import Reflex.Dom
 import qualified Data.Text as T
 import qualified Data.Map as M
 import qualified Data.Tree
-import qualified Data.Monoid ((<>))
+import Data.Monoid ((<>))
 
 data Prop = NewProp T.Text | Apply T.Text Prop deriving (Eq, Show)
 
-select :: MonadWidget t m => Prop -> m (Dynamic t Prop)
-select (NewProp t) = el "span" (text t) >> constDyn (NewProp t)
-select (Apply t p)@a = mdo
-  (outer, (inner, _)) <- elDynAttr' "span" (M.singleton "background-color" <$> color) $ do
+selectProp :: MonadWidget t m => Prop -> m (Dynamic t Prop)
+selectProp p@(NewProp t) = el "span" (text t) >> return (constDyn p)
+selectProp a@(Apply t p) = mdo
+  (outerEl, innerDyn) <- elDynAttr' "span" (M.singleton "style" <$> color) $ do
     text t
-    el' "small" (select p)
-  holdDyn a ((p <$ innerEv) <> (a <$ outerEv))
-  color <- holdDyn "#FFFFFF" (("#FFFFFF" <$ innerEv) <> ("#CCCCCC" <$ outerEv))
+    text "("
+    d <- selectProp p
+    text ")"
+    return d
+  let outerEv = domEvent Click outerEl
+  let innerEv = updated innerDyn
+  color <- holdDyn "background-color: #CCCCCC;" $ leftmost ["background-color: #FFFFFF;" <$ innerEv, "background-color: #CCCCCC;" <$ outerEv]
+  holdDyn a $ leftmost [p <$ innerEv, a <$ outerEv]
 
 page :: MonadWidget t m => m (Event t T.Text)
 page = do
-  h2t = el "h2" . text
+  let h2t = el "h2" . text
   h2t "toNat :: Prop → nat"
   h2t "toProp :: nat → Prop"
-  h2t "proof n m := m is proof for <n"
-  h2t "provable n := exists m. Proof n m"
-  select $ Apply "toNat" $ Apply "Provable" $ NewProp "P"
+  h2t "Proof n m := m is proof for (toNat n)"
+  h2t "Provable n := exists m. Proof n m"
+  p <- elAttr "p" (M.singleton "style" "font-size: 42px;") $ selectProp $ Apply "toNat" $ Apply "Provable" $ NewProp "P"
+  display p
+  return never
