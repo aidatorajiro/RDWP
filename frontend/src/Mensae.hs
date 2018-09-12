c8aba943451e618dbd50ea64f58755af82fcb99e
aidatorajiro <kawarusosu@zoho.com>
2018-09-12 21:23:13 +0900

    update Mensae

--- frontend/src/Mensae.hs
+++ frontend/src/Mensae.hs
@@ -6,26 +6,15 @@ module Mensae ( page ) where
 import Reflex.Dom
 import qualified Data.Text as T
 import qualified Data.Map as M
-import qualified Data.Tree
+import Data.Tree
 import Data.Monoid ((<>))
 import Elements
 
-data Func = NewFunc T.Text | Apply T.Text Func deriving (Eq, Show)
-
-{-
-selectFunc :: MonadWidget t m => Func -> m (Dynamic t Func)
-selectFunc p@(NewFunc t) = do
-  (span, _) <- el' "span" $ text t
-  holdDyn p (p <$ domEvent Click span)
-
-selectFunc a@(Apply t p) = mdo
-  (outerEl, innerEv) <- elDynStyle' "span" $ do
-    text t
-    updated <$> selectFunc p
-  let outerEv = domEvent Click outerEl
-  styleDyn <- holdDyn "background: #CCC;" $ leftmost ["background: transparent;" <$ innerEv, "background: #CCC;" <$ outerEv]
-  holdDyn a $ leftmost [innerEv, a <$ outerEv]
--}
+selectTree :: MonadWidget t m => Tree T.Text -> m (Event t (Tree T.Text))
+selectTree = foldTree (\txt evs -> do
+    (e, _) <- el' "span" (text txt)
+    leftmost ((txt <$ domEvent Click e) : evs)
+  )
 
 page :: MonadWidget t m => m (Event t T.Text)
 page = do
@@ -34,6 +23,6 @@ page = do
   h2t "toProp :: nat → Prop"
   h2t "Proof n m := m is proof for (toProp n)"
   h2t "Provable n := exists m. Proof n m"
-  --p <- elStyle "p" "font-size: 42px;" $ selectFunc $ Apply "toNat" $ Apply "Provable" $ Apply "toNat" $ NewFunc "P"
-  --display p
+  p <- elStyle "p" "font-size: 42px;" $ selectTree $ Node "Proof" [Node "toNat" [Node "P" []]]
+  trace p
   return never
