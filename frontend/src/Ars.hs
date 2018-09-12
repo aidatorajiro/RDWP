aff02c0ab84387678c0b8b4bbb2f58adc2cc65bb
aidatorajiro <kawarusosu@zoho.com>
2018-09-12 20:23:29 +0900

    update style

--- frontend/src/Ars.hs
+++ frontend/src/Ars.hs
@@ -11,7 +11,7 @@ message :: MonadWidget t m => T.Text -> m (Event t ())
 message txt = do
   tickCount <- count =<< getTickEv 0.1
   let dt = fmap (\n -> T.take n txt) tickCount
-  (e, _) <- elStyle' "span" "margin-top: 100px; font-size: 20px;" (dynText dt)
+  (e, _) <- elStyle' "div" "padding-top: 100px; font-size: 20px; text-align: center; height: 100%; width: 100%;" (dynText dt)
   return (domEvent Click e)
 
 page :: MonadWidget t m => Int -> m (Event t T.Text)
@@ -19,7 +19,7 @@ page n = do
   ev <- case n of 
     0 -> do
       elStyle "h1" "margin-top: 100px;" (text "Welcome To Arsnet")
-      (e, _) <- elStyle' "h2" "margin-top: 100px; color: #231497;" (text "ENTER")
+      (e, _) <- elStyle' "h2" "margin-top: 100px; color: #142366;" (text "ENTER")
       return (domEvent Click e)
     1 -> message "Congratulations."
     2 -> message ""
