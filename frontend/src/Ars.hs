d42a75e152fb590aa3ea24b15955a1d412724b32
aidatorajiro <kawarusosu@zoho.com>
2018-09-12 23:02:52 +0900

    modifie style

--- frontend/src/Ars.hs
+++ frontend/src/Ars.hs
@@ -11,15 +11,16 @@ message :: MonadWidget t m => T.Text -> m (Event t ())
 message txt = do
   tickCount <- count =<< getTickEv 0.1
   let dt = fmap (\n -> T.take n txt) tickCount
-  (e, _) <- elStyle' "div" "padding-top: 100px; font-size: 20px; text-align: center; height: 100%; width: 100%;" (dynText dt)
+  (e, _) <- elStyle' "div" "padding-top: 100px; font-size: 20px; height: 100%; width: 100%;" (dynText dt)
   return (domEvent Click e)
 
 page :: MonadWidget t m => Int -> m (Event t T.Text)
 page n = do
+  el "style" $ text "body { text-align: center; background: #821e0a; margin: 0; padding: 0; color: #ffffff; font-family: \"Times New Roman\", serif; }"
   ev <- case n of 
     0 -> do
       elStyle "h1" "margin-top: 100px;" (text "Welcome To Arsnet")
-      (e, _) <- elStyle' "h2" "margin-top: 100px; color: #142366;" (text "ENTER")
+      (e, _) <- elStyle' "h2" "margin-top: 100px; color: #F3C3C3;" (text "ENTER")
       return (domEvent Click e)
     1 -> message "Congratulations."
     2 -> message ""
