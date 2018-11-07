c07221dc334ad25fff5fe1c15ddc2a248f52a85b
aidatorajiro <kawarusosu@zoho.com>
2018-11-07 22:40:15 +0900

    embed FakeIndex.css to FakeIndex.hs

--- frontend/src/FakeIndex.hs
+++ frontend/src/FakeIndex.hs
@@ -1,18 +1,64 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module FakeIndex ( page ) where
 
 import qualified Data.ByteString as B
 import qualified Data.Text as T
 import qualified Data.Map as M
+import Text.RawString.QQ
 
 import Reflex.Dom
-import Elements (h1', spanAbsP)
-
-import Data.FileEmbed (embedFile)
+import Elements (h1ID', spanAbsP)
 
 import Data.Monoid ((<>))
+
+css :: T.Text
+css = [r|
+*::selection {
+  color: transparent;
+	text-shadow: 0 0 8px #000;
+}
+
+#bind {
+  text-align: center;
+  font-size: 40px;
+  width: 100px;
+  margin: auto;
+}
+
+#dollar {
+  font-size: 40px;
+  position: absolute;
+  left:10px;
+  top: calc(50% - 70px);
+  width:30px;
+  height: 100px;
+  line-height: 100px;
+  margin: 0;
+}
+
+#fmap {
+  font-size: 40px;
+  position: absolute;
+  right:10px;
+  top: calc(50% - 30px);
+  width:30px;
+  height: 100px;
+  line-height: 100px;
+  margin: 0;
+}
+
+#strict {
+  text-align: center;
+  font-size: 40px;
+  position: absolute;
+  bottom: 0px;
+  left: calc(50% - 15px);
+  width: 30px;
+  margin: auto;
+}
+|]
 
 data SpanType = Dollar | BindL | BindR
 
@@ -64,26 +110,23 @@ spanList = [
   (BindL, 81, 68),
   (BindL, 72, 57)]
 
-{-
-css :: B.ByteString
-css = $(embedFile "assets/css/FakeIndex.css")
--}
-
 -- PATH TO THE DEPTH
 -- worry skate notice member person slender indicate fun urge chalk foster fiber chunk inch popular
 page :: MonadWidget t m => m (Event t T.Text)
-page = el "div" $ do
-  mapM_ (\(t, x, y) -> spanAbsP (spanText t) x y) spanList
-  spanAbsP "$" 10 10
-  
-  bind   <- h1' "=<<"
-  dollar <- h1' "$"
-  fmap   <- h1' "<$>"
-  strict <- h1' "!"
-  
-  return $ leftmost [
-      "/nmnmnmnmn" <$ domEvent Click bind,
-      "/nazo"      <$ domEvent Click dollar,
-      "/harituke"  <$ domEvent Click fmap,
-      "/worry"     <$ domEvent Click strict
-    ]
+page = do
+  el "style" $ text css
+  el "div" $ do
+    mapM_ (\(t, x, y) -> spanAbsP (spanText t) x y) spanList
+    spanAbsP "$" 10 10
+    
+    bind   <- h1ID' "bind" "=<<"
+    dollar <- h1ID' "dollar" "$"
+    fmap   <- h1ID' "fmap" "<$>"
+    strict <- h1ID' "strict" "!"
+    
+    return $ leftmost [
+        "/nmnmnmnmn" <$ domEvent Click bind,
+        "/nazo"      <$ domEvent Click dollar,
+        "/harituke"  <$ domEvent Click fmap,
+        "/worry"     <$ domEvent Click strict
+      ]
