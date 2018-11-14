412f5c4ff7f0de8307e67c82449670cdd7abbc6d
Aida Torajiro <kawarusosu@zoho.com>
2018-11-14 13:38:00 +0900

    Add buttons in Mn1

--- frontend/src/Mn1.hs
+++ frontend/src/Mn1.hs
@@ -4,6 +4,15 @@ module Mn1 ( page ) where
 
 import Reflex.Dom
 import qualified Data.Text as T
+import Elements
 
 page :: MonadWidget t m => m (Event t T.Text)
-page = return never
+page = do
+    el "style" $ text "body{background:#2f1371;}"
+    let stl = "cursor: pointer; background: #efb5db;"
+    let btn = fmap (domEvent Click . fst) . elStyle' "div" stl . text 
+    btnLeft <- btn "←"
+    btnUp <- btn "↑"
+    btnDown <- btn "↓"
+    btnRight <- btn "→"
+    
