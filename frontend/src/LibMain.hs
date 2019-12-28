08b015f0df863dce67f4a7a999dbc8460d770837
aidatorajiro <kawarusosu@zoho.com>
2019-12-28 12:44:33 +0900

    Introduce reset css

--- frontend/src/LibMain.hs
+++ frontend/src/LibMain.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE RecursiveDo, OverloadedStrings, FlexibleContexts, CPP #-}
+{-# LANGUAGE RecursiveDo, OverloadedStrings, FlexibleContexts, CPP, QuasiQuotes #-}
 
 module LibMain ( startApp ) where
 
@@ -22,6 +22,118 @@ import qualified Mn1
 import qualified Ars
 import qualified ArsGame
 import qualified IClub
+import Elements
+import Text.RawString.QQ
+
+reset_css :: T.Text
+reset_css = [r|
+  /*
+  html5doctor.com Reset Stylesheet
+  v1.6.1
+  Last Updated: 2010-09-17
+  Author: Richard Clark - http://richclarkdesign.com
+  Twitter: @rich_clark
+  */
+  
+  html, body, div, span, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  abbr, address, cite, code,
+  del, dfn, em, img, ins, kbd, q, samp,
+  small, strong, sub, sup, var,
+  b, i,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, figcaption, figure,
+  footer, header, hgroup, menu, nav, section, summary,
+  time, mark, audio, video {
+      margin:0;
+      padding:0;
+      border:0;
+      outline:0;
+      font-size:100%;
+      vertical-align:baseline;
+      background:transparent;
+  }
+  
+  body {
+      line-height:1;
+  }
+  
+  article,aside,details,figcaption,figure,
+  footer,header,hgroup,menu,nav,section {
+      display:block;
+  }
+  
+  nav ul {
+      list-style:none;
+  }
+  
+  blockquote, q {
+      quotes:none;
+  }
+  
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+      content:'';
+      content:none;
+  }
+  
+  a {
+      margin:0;
+      padding:0;
+      font-size:100%;
+      vertical-align:baseline;
+      background:transparent;
+  }
+  
+  /* change colours to suit your needs */
+  ins {
+      background-color:#ff9;
+      color:#000;
+      text-decoration:none;
+  }
+  
+  /* change colours to suit your needs */
+  mark {
+      background-color:#ff9;
+      color:#000;
+      font-style:italic;
+      font-weight:bold;
+  }
+  
+  del {
+      text-decoration: line-through;
+  }
+  
+  abbr[title], dfn[title] {
+      border-bottom:1px dotted;
+      cursor:help;
+  }
+  
+  table {
+      border-collapse:collapse;
+      border-spacing:0;
+  }
+  
+  /* change border colour to suit your needs */
+  hr {
+      display:block;
+      height:1px;
+      border:0;  
+      border-top:1px solid #cccccc;
+      margin:1em 0;
+      padding:0;
+  }
+  
+  input, select {
+      vertical-align:middle;
+  }
+
+  body {
+    overflow: hidden;
+  }
+|]
 
 -- | parse URL Location Path
 parseLocationPath :: MonadWidget t m => Parsec T.Text () (m (Event t T.Text))
@@ -60,6 +172,9 @@ popState = do
 -- | start the app
 startApp :: IO ()
 startApp = mainWidget $ mdo
+  -- set reset css
+  style reset_css
+
   -- Get an Event of Event which contains dynamically changing widget.
   ee <- dyn $ router <$> loc
 
