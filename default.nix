cd050f75b5800dccb5fde261579bca6f994acec7
aidatorajiro <kawarusosu@zoho.com>
2021-02-22 20:13:16 +0900

    Update reflex-platform

--- default.nix
+++ default.nix
@@ -7,16 +7,13 @@
   shells = {
     ghc = ["frontend"];
     ghcjs = ["frontend"];
-    # wasm = ["frontend"];
+    wasm = ["frontend"];
   };
-  shellToolOverrides = ghc: super: {
-    haskell-language-server = unstable.haskell-language-server;
+  overrides = self: super: {
+    mmorph = self.callHackage "mmorph" "1.1.3" {}; # special hack for mmorph
+    matrix = pkgs.haskell.lib.dontCheck super.matrix; # immunity from tests
+    reflex = pkgs.haskell.lib.dontCheck super.reflex; # immunity from tests
+    reflex-dom-core = pkgs.haskell.lib.dontCheck super.reflex-dom-core; # immunity from tests
+    loop = pkgs.haskell.lib.dontCheck super.loop; # immunity from tests
   };
-  # for wasm config
-  #overrides = self: super: {
-  #  mmorph = self.callHackage "mmorph" "1.1.3" {}; # special hack for mmorph
-  #  matrix = pkgs.haskell.lib.dontCheck super.matrix; # immunity from tests
-  #  reflex = pkgs.haskell.lib.dontCheck super.reflex; # immunity from tests
-  #  reflex-dom-core = pkgs.haskell.lib.dontCheck super.reflex-dom-core; # immunity from tests
-  #};
 })
