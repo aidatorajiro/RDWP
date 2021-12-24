ffbd31905e0c7fb90e30863cfe339eae813915f4
aidatorajiro <kawarusosu@zoho.com>
2021-12-25 00:34:02 +0900

    add some build/run script

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+const fs = require('fs');
 
 app.get('*', function (request, response) {
   // TODO: if no file found, use index.html. otherwise, use the file.
-  response.sendFile(path.join(__dirname, 'result/ghcjs/frontend/bin/RDWP-exe.jsexe', 'index.html'));
+  console.log(request.path)
+  let p = path.join(__dirname, 'RDWP-exe.jsexe', request.path);
+  console.log(p)
+  if (!fs.existsSync(p)) {
+    response.sendFile(path.join(__dirname, 'RDWP-exe.jsexe', 'index.html'))
+  } else {
+    response.sendFile(p)
+  }
 });
 
 app.listen(port);
