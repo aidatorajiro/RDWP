bbc120daf4fb99a84026153656ee17062162a9e9
aidatorajiro <kawarusosu@zoho.com>
2021-12-25 11:36:25 +0900

    update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,4 @@
-docker cp ./frontend mycontainer:/home/nix/RDWP
+docker cp ./ mycontainer:/home/nix/RDWP
 docker exec mycontainer sh -c ". ~/e.sh; sudo rm -rf frontend/.stack-work; nix-build -A ghcjs.RDWP"
 sudo docker cp mycontainer:/home/nix/RDWP/result/bin/RDWP-exe.jsexe ./
 sudo cp RDWP-exe.jsexe/index.html RDWP-exe.jsexe/404.html
