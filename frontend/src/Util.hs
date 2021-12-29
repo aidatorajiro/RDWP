b995c5065f03bd1cd350130a06dfb8a346859f0e
aidatorajiro <kawarusosu@zoho.com>
2021-12-30 01:21:27 +0900

    add comment in Util

--- frontend/src/Util.hs
+++ frontend/src/Util.hs
@@ -12,14 +12,16 @@ import Reflex.Dom
 import Language.Javascript.JSaddle (JSM, JSVal, jsg, (#))
 import Control.Monad (void)
 
+-- | get ticking event
 getTickEv :: MonadWidget t m => NominalDiffTime -> m (Event t TickInfo)
 getTickEv d = do
   now <- liftIO getCurrentTime
   tickLossy d now
 
--- dynamic random number and random number generator with given range and initial generator
+-- | dynamic random number and random number generator with given range and initial generator
 randomRDyn :: (Random a, RandomGen g, MonadWidget t m) => (a, a) -> g -> Event t b -> m (Dynamic t a, Dynamic t g)
 randomRDyn range initGen ev = splitDynPure <$> foldDyn (\_ (_, g) -> randomR range g) (randomR range initGen) ev
 
+-- | run console.log
 debugConsoleLog :: JSVal -> JSM ()
 debugConsoleLog a = void $ jsg ("console" :: String) # ("log" :: String) $ [a]
