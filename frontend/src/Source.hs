9d99cb96daa39802a01cf380fde4544bede2f86a
aidatorajiro <kawarusosu@zoho.com>
2021-12-30 01:20:34 +0900

    add page Source

--- frontend/src/Source.hs
+++ frontend/src/Source.hs
@@ -1,2 +1,21 @@
-module Source where
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 
+module Source (page) where
+
+-- Source.hs, a.k.a. code forest, a.k.a. /cf1*
+-- neta: cf1 ... bitcoin address ...
+
+import Reflex.Dom
+import qualified Data.Text as T
+import Elements
+import Text.RawString.QQ
+
+page :: MonadWidget t m => String -> m (Event t T.Text)
+page s = do
+    style [r|
+body {
+    background: #120317;
+}
+|]
+    return never
