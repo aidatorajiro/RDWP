b4da741242828c241923bdf30106ffbf5d596b4e
aidatorajiro <kawarusosu@zoho.com>
2022-02-21 23:29:57 +0900

    update reflex-platform and remove default.nix weird trick

--- default.nix
+++ default.nix
@@ -1,6 +1,5 @@
 {system ? builtins.currentSystem, unstable ? import <nixos-unstable> {}} :
 (import ./reflex-platform { inherit system; }).project ({pkgs, ...}: {
-  useWarp = true;
   packages = {
     RDWP = ./frontend;
   };
@@ -8,12 +7,5 @@
     ghc = ["frontend"];
     ghcjs = ["frontend"];
     wasm = ["frontend"];
-  };
-  overrides = self: super: {
-    mmorph = self.callHackage "mmorph" "1.1.3" {}; # special hack for mmorph
-    matrix = pkgs.haskell.lib.dontCheck super.matrix; # immunity from tests
-    reflex = pkgs.haskell.lib.dontCheck super.reflex; # immunity from tests
-    reflex-dom-core = pkgs.haskell.lib.dontCheck super.reflex-dom-core; # immunity from tests
-    loop = pkgs.haskell.lib.dontCheck super.loop; # immunity from tests
   };
 })
