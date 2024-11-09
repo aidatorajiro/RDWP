4357ca774c1d96fc9e8027a7648ff560020d320c
aidatorajiro <kawarusosu@zoho.com>
2024-11-10 01:24:46 +0900

    Update reflex; Fix build.sh;

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
-sudo rm -rf RDWP-exe.jsexe
+rm -rf RDWP-exe.jsexe
 docker start mycontainer
+docker exec mycontainer sh -c "sudo rm -rf /home/nix/RDWP"
 docker cp ./ mycontainer:/home/nix/RDWP
-docker exec mycontainer sh -c ". ~/e.sh; sudo rm -rf frontend/.stack-work result; nix-build -A ghcjs.RDWP"
-sudo docker cp mycontainer:/home/nix/RDWP/result/bin/RDWP-exe.jsexe ./
-sudo cp RDWP-exe.jsexe/index.html RDWP-exe.jsexe/404.html
-sudo cp frontend/assets/* RDWP-exe.jsexe
-sudo chmod -R 755 RDWP-exe.jsexe
-sudo chown -R $USER RDWP-exe.jsexe
+docker exec mycontainer sh -c "sudo chown -R nix:nix /home/nix/RDWP"
+docker exec mycontainer sh -c ". ~/e.sh; sudo rm -rf frontend/.stack-work result; nix-build -A ghcjs.RDWP; cd result/bin; tar cvf /home/nix/RDWP/jsexe.tar RDWP-exe.jsexe"
+docker cp mycontainer:/home/nix/RDWP/jsexe.tar ./
+tar xvf jsexe.tar
+chmod -R 755 RDWP-exe.jsexe
+cp RDWP-exe.jsexe/index.html RDWP-exe.jsexe/404.html
+cp frontend/assets/* RDWP-exe.jsexe
+rm jsexe.tar
