74188beea605e421ceada7dd2414fa5df73609a4
aidatorajiro <kawarusosu@zoho.com>
2024-11-11 17:27:03 +0900

    proper removal of previous build files

--- build-wasm.sh
+++ build-wasm.sh
@@ -3,7 +3,7 @@ docker start mycontainer
 docker exec mycontainer sh -c "sudo rm -rf /home/nix/RDWP"
 docker cp ./ mycontainer:/home/nix/RDWP
 docker exec mycontainer sh -c "sudo chown -R nix:nix /home/nix/RDWP"
-docker exec mycontainer sh -c ". ~/e.sh; sudo rm -rf frontend/.stack-work result result-1 result-2 result-copy; nix-build default-wasm.nix -A wasm.RDWP -o result-1; nix-build release-wasm.nix -A wasm-app -o result-2; sudo cp -rL result-2/ result-wasm/; tar cvf result-wasm.tar result-wasm"
+docker exec mycontainer sh -c ". ~/e.sh; sudo rm -rf .stack-work frontend/.stack-work result result-1 result-2 result-wasm RDWP-exe.jsexe result-wasm.tar jsexe.tar; nix-build default-wasm.nix -A wasm.RDWP -o result-1; nix-build release-wasm.nix -A wasm-app -o result-2; sudo cp -rL result-2/ result-wasm/; tar cvf result-wasm.tar result-wasm"
 docker cp mycontainer:/home/nix/RDWP/result-wasm.tar .
 tar xvf result-wasm.tar
 chmod -R 755 result-wasm
--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ docker start mycontainer
 docker exec mycontainer sh -c "sudo rm -rf /home/nix/RDWP"
 docker cp ./ mycontainer:/home/nix/RDWP
 docker exec mycontainer sh -c "sudo chown -R nix:nix /home/nix/RDWP"
-docker exec mycontainer sh -c ". ~/e.sh; sudo rm -rf frontend/.stack-work result; nix-build -A ghcjs.RDWP; cd result/bin; tar cvf /home/nix/RDWP/jsexe.tar RDWP-exe.jsexe"
+docker exec mycontainer sh -c ". ~/e.sh; sudo rm -rf .stack-work frontend/.stack-work result result-1 result-2 result-wasm RDWP-exe.jsexe result-wasm.tar jsexe.tar; nix-build -A ghcjs.RDWP; cd result/bin; tar cvf /home/nix/RDWP/jsexe.tar RDWP-exe.jsexe"
 docker cp mycontainer:/home/nix/RDWP/jsexe.tar ./
 tar xvf jsexe.tar
 chmod -R 755 RDWP-exe.jsexe
