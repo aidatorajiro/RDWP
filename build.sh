28625fa4d01cbaab07e368151964bf6807f96cc8
aidatorajiro <kawarusosu@zoho.com>
2024-11-12 17:53:11 +0900

    fix build script (reduce copy amount)

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 rm -rf RDWP-exe.jsexe
 docker start mycontainer
-docker exec mycontainer sh -c "sudo rm -rf /home/nix/RDWP"
-docker cp ./ mycontainer:/home/nix/RDWP
+docker exec mycontainer sh -c "sudo rm -rf /home/nix/RDWP; mkdir /home/nix/RDWP"
+docker cp ./frontend mycontainer:/home/nix/RDWP
+docker cp ./reflex-platform mycontainer:/home/nix/RDWP
+docker cp ./stack.yaml mycontainer:/home/nix/RDWP
+docker cp ./stack.yaml.lock mycontainer:/home/nix/RDWP
+docker cp ./default.nix mycontainer:/home/nix/RDWP
 docker exec mycontainer sh -c "sudo chown -R nix:nix /home/nix/RDWP"
 docker exec mycontainer sh -c ". ~/e.sh; sudo rm -rf .stack-work frontend/.stack-work result result-1 result-2 result-wasm RDWP-exe.jsexe result-wasm.tar jsexe.tar; nix-build -A ghcjs.RDWP; cd result/bin; tar cvf /home/nix/RDWP/jsexe.tar RDWP-exe.jsexe"
 docker cp mycontainer:/home/nix/RDWP/jsexe.tar ./
