9b7856312ecf5381f9308b09930d3176ca0a148d
aidatorajiro <kawarusosu@zoho.com>
2024-11-17 21:53:41 +0900

    fix upload.sh

--- upload.bash
+++ upload.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 cd RDWP-exe.jsexe
+rm -rf .git
 git init
+git branch -m main
 echo '.DS_Store' > .gitignore
 git remote add origin git@github.com:aidatorajiro/RDWP-dist
 git add .
 git commit -m 'Initial commit'
-git push --force origin master
+git push --force origin main
