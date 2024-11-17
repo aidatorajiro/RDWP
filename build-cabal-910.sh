b16de8cc31dcc44932b66b1dab1bf335b3cc236f
aidatorajiro <kawarusosu@zoho.com>
2024-11-17 21:12:23 +0900

    simpler solution for hschs patch

--- build-cabal-910.sh
+++ build-cabal-910.sh
@@ -1,9 +1,7 @@
-cp ./build-tools/patch-ghcup/hsc2hs-ghc ~/.ghcup/ghc/javascript-unknown-ghcjs-9.10.0.20240413/bin/hsc2hs-ghc-9.10.0.20240413
-
 rm -rf RDWP-exe.jsexe result
 
 pushd frontend
-cabal build --project-file=./project-file-910
+cabal build --project-file=./project-file-910 --with-hsc2hs=javascript-unknown-ghcjs-hsc2hs-9.10.0.20240413
 cp -r dist-newstyle/build/javascript-ghcjs/ghc-9.10.*/RDWP-*/x/RDWP-exe/build/RDWP-exe/RDWP-exe.jsexe ../RDWP-exe.jsexe
 popd
 cp RDWP-exe.jsexe/index.html RDWP-exe.jsexe/404.html
