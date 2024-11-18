7ae0ee93b4f42ec2497651fb16b11dfdcf467aa0
aidatorajiro <kawarusosu@zoho.com>
2024-11-18 17:06:00 +0900

    https key for upload

--- upload.bash
+++ upload.bash
@@ -5,7 +5,13 @@ rm -rf .git
 git init
 git branch -m main
 echo '.DS_Store' > .gitignore
-git remote add origin git@github.com:aidatorajiro/RDWP-dist
+
+if [ "$(git config credential.helper)" == "store" ]; then
+  git remote add origin https://github.com/aidatorajiro/RDWP-dist
+else
+  git remote add origin git@github.com:aidatorajiro/RDWP-dist
+fi
+
 git add .
 git commit -m 'Initial commit'
 git push --force origin main
