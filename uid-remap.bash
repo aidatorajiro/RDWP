4af01c2e786ce02bbcd359d053742411f0783dd6
aidatorajiro <kawarusosu@zoho.com>
2024-11-26 23:49:53 +0900

    fix uid remap

--- uid-remap.bash
+++ uid-remap.bash
@@ -11,13 +11,17 @@ if [ -d "/workspace" ]; then
     if [ "$uid" -ne 0 ]; then
         if [ "$(id -g $USER)" -ne $gid ]; then
             getent group $gid >/dev/null 2>&1 || groupmod -g $gid $USER
-            chgrp -R $gid $HOME
         fi
         if [ "$(id -u $USER)" -ne $uid ]; then
             usermod -u $uid $USER
         fi
     fi
 
+    ghcup_uid=$(stat -c "%u" $HOME/.ghcup)
+    ghcup_gid=$(stat -c "%g" $HOME/.ghcup)
+    if [ $ghcup_uid -ne $gid ] || [ $ghcup_gid -ne $uid ]; then
+        chown -R $gid:$uid $HOME
+    fi
 fi
 
 exec setpriv --reuid=$USER --regid=$USER --init-groups "$@"
