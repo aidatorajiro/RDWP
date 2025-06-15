d00d7c9f110e2294f0c72e0e629a2a8ab2b50720
tora <kawarusosu@zoho.com>
2025-06-16 04:20:59 +0900

    fix ci; rename build scripts;

--- build-cabal-jsbackend.bash
+++ build-cabal-jsbackend.bash
@@ -4,7 +4,7 @@ set -euo pipefail
 rm -rf RDWP-exe.jsexe result
 
 pushd frontend
-cabal build --project-file=./project-file-910 --with-hsc2hs=javascript-unknown-ghcjs-hsc2hs-9.12.2
+cabal build --project-file=./project-file-jsbackend --with-hsc2hs=javascript-unknown-ghcjs-hsc2hs-9.12.2
 cp -r dist-newstyle/build/javascript-ghcjs/ghc-9.12.2/RDWP-*/x/RDWP-exe/build/RDWP-exe/RDWP-exe.jsexe ../RDWP-exe.jsexe
 popd
 cp index-ghcjs/index.html RDWP-exe.jsexe/index.html
